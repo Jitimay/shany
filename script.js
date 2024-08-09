eab9e439c4390ea70a3664053490eb50b3ae9cf3
Josh <josh@josh-ThinkPad-T460s.mshome.net>
2024-08-09 02:04:57 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkCode() {
     const enteredCode = document.getElementById('codeInput').value;
-    const correctCode = 'your-secret-code';  // Replace this with the actual code
+    const correctCode = '354262';  // Replace this with the actual code
 
     if (enteredCode === correctCode) {
         document.getElementById('login-section').style.display = 'none';
